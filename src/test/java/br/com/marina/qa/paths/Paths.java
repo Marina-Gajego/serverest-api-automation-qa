efd3330d6110c73b5b928fed5f2da4ca86d7937d
Marina <marinagajego1377@gmail.com>
2026-03-23 21:36:43 -0300

    Criando a estrutura do projeto de automação

---
+++ src/test/java/br/com/marina/qa/paths/Paths.java
@@ -0,0 +1,9 @@
+package br.com.marina.qa.paths;
+
+public class Paths {
+    public static final String BASE_URL = "https://serverest.dev";
+
+    // Endpoints
+    public static final String AUTH_ENDPOINT = "/login";
+    public static final String USERS_ENDPOINT = "/usuarios";
+}
